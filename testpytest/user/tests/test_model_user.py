54764d4200e1e5d1d3244603fe3f2e475270d0f2
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2022-01-15 21:08:53 -0300

    Dev (#3)
    * add flake8 e pytest config files
    
    * moved flak8 to flake8
    
    * add app user
    
    * add model , model bakery and coverage in test

---
+++ testpytest/user/tests/test_model_user.py
@@ -0,0 +1,19 @@
+import pytest
+import pytest_django
+from model_bakery import baker
+from testpytest.user.models import User
+
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user()
+    assert User.objects.count() == 1
+
+@pytest.fixture
+def user(db):
+    return baker.make(User)
+
+def test_user(user):
+    assert user.pk == 1
+    
+
